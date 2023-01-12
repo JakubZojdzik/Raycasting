b1b7fd11308d2b9ab78359ec535b0d4001880607
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-01-12 22:50:16 +0100

    Fix computing distances

--- main.cpp
+++ main.cpp
@@ -10,53 +10,39 @@
 #include <pthread.h>
 
 // ----------------------  CONTROLS  ----------------------
-const int MAP_WIDTH =14;
-const int MAP_HEIGHT = 14;
+const int MAP_WIDTH = 8;
+const int MAP_HEIGHT = 8;
 int board[MAP_WIDTH][MAP_HEIGHT] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}
 };
-const int SCREEN_WIDTH = 220;
+const int SCREEN_WIDTH = 240;
 const int SCREEN_HEIGHT = 50;
-double posX = 1, posY = 3;
+double posX = 2.5, posY = 2.5;
 double direction = 90;
-const double MOVEMENT_SPEED = 1;
+const double MOVEMENT_SPEED = .4;
 const double ROTATION_SPEED = 4.0;
-const double ANGLE_INCREMENT = 1.0;
 // --------------------------------------------------------
 
 const double FOV = SCREEN_WIDTH / 2;
 int screen[SCREEN_WIDTH][SCREEN_HEIGHT];
 struct termios oldSettings, newSettings;
 
-
 /*
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}
 
 {'@', '%', '#', '*', '+', '=', '-', ':', '.'} // 9 chars
 {'$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', '\'', '.'} // 68 chars
@@ -65,7 +51,7 @@ struct termios oldSettings, newSettings;
 */
 char charset[] = {'@', '%', '=', ':', '.'};
 const int COLORS = 5;
-char ceilC = '_', floorC = '_';
+char ceilC = ' ', floorC = ' ';
 
 double to_rad(double degree)
 {
@@ -81,7 +67,7 @@ void normalize_angle(double &angle)
 
 void *listen(void *vargp)
 {
-    while ( 1 )
+    while(1)
     {
         fd_set set;
         struct timeval tv;
@@ -89,15 +75,15 @@ void *listen(void *vargp)
         tv.tv_sec = 10;
         tv.tv_usec = 0;
 
-        FD_ZERO( &set );
-        FD_SET( fileno( stdin ), &set );
+        FD_ZERO(&set);
+        FD_SET(fileno(stdin), &set);
 
-        int res = select( fileno( stdin )+1, &set, NULL, NULL, &tv );
+        int res = select(fileno(stdin) + 1, &set, NULL, NULL, &tv);
 
-        if( res > 0 )
+        if(res > 0)
         {
             char c;
-            read( fileno( stdin ), &c, 1 );
+            read(fileno( stdin ), &c, 1);
             double stepDir = direction;
             double incX, incY;
             switch (c)
@@ -110,6 +96,7 @@ void *listen(void *vargp)
                 {
                     posX += incX * MOVEMENT_SPEED;
                     posY += incY * MOVEMENT_SPEED;
+                    break;
                 }
                 break;
 
@@ -126,18 +113,6 @@ void *listen(void *vargp)
                 break;
 
             case 'd':
-                stepDir += 90;
-                normalize_angle(stepDir);
-                incX = cos(to_rad(stepDir));
-                incY = sin(to_rad(stepDir));
-                if(!board[long(posX+incX)][long(posY+incY)])
-                {
-                    posX += incX * MOVEMENT_SPEED;
-                    posY += incY * MOVEMENT_SPEED;
-                }
-                break;
-
-            case 'a':
                 stepDir -= 90;
                 normalize_angle(stepDir);
                 incX = cos(to_rad(stepDir));
@@ -149,16 +124,26 @@ void *listen(void *vargp)
                 }
                 break;
 
+            case 'a':
+                stepDir += 90;
+                normalize_angle(stepDir);
+                incX = cos(to_rad(stepDir));
+                incY = sin(to_rad(stepDir));
+                if(!board[long(posX+incX)][long(posY+incY)])
+                {
+                    posX += incX * MOVEMENT_SPEED;
+                    posY += incY * MOVEMENT_SPEED;
+                }
+                break;
+
             case 'l':
-                direction += ROTATION_SPEED;
+                direction -= ROTATION_SPEED;
                 normalize_angle(direction);
                 break;
 
             case 'k':
-                direction -= ROTATION_SPEED;
+                direction += ROTATION_SPEED;
                 normalize_angle(direction);
-                break;
-            default:
                 break;
             }
         }
@@ -170,7 +155,7 @@ void *listen(void *vargp)
 void draw()
 {
     std::cout << "\033[0;0H";
-    for(int y = 0; y < SCREEN_HEIGHT; y++)
+    for(int y = SCREEN_HEIGHT-1; y >= 0; y--)
     {
         for(int x = 0; x < SCREEN_WIDTH; x++)
         {
@@ -189,7 +174,20 @@ void draw()
         }
         std::cout << "\n";
     }
-    std::cout << "(" << posX << ", " << posY << "), " << direction << "      \n";
+    std::cout << "(" << posX << ", " << posY << "), " << direction << "                                \n";
+    for(int y = MAP_HEIGHT-1; y >= 0; y--)
+    {
+        for(int x = 0; x < MAP_WIDTH; x++)
+        {
+            if(y == long(posY) && x == long(posX))
+                std::cout << '#';
+            else if(board[x][y])
+                std::cout << board[x][y];
+            else
+                std::cout << '.';
+        }
+        std::cout << '\n';
+    }
 }
 
 void setup()
@@ -210,35 +208,35 @@ void *game_loop(void *vargp)
         int line = 0;
         double curr_dir = direction;
         double curr_posX = posX, curr_posY = posY;
-        for(double degitr = curr_dir - FOV/2; degitr <= curr_dir + FOV/2; degitr += ANGLE_INCREMENT)
+        for(double degitr = curr_dir + FOV/2; degitr >= curr_dir - FOV/2; degitr--)
         {
             double deg = degitr;
             normalize_angle(deg);
             double currX, currY, fullX, fullY, syf;
-            currX = abs((1 - modf(curr_posX, &syf)) / cos(to_rad(deg)));
-            currY = abs((1 - modf(curr_posY, &syf)) / sin(to_rad(deg)));
             fullX = abs(1 / cos(to_rad(deg)));
             fullY = abs(1 / sin(to_rad(deg)));
-            // if(deg == 0 || deg == 180)
-            // {
-            //     currX = abs(1 - modf(curr_posX, &syf));
-            //     fullX = 1;
-            //     currY = 1e17;
-            //     fullY = 1e17;
-            // }
-            // if(deg == 90 || deg == 270)
-            // {
-            //     currX = 1e17;
-            //     fullX = 1e17;
-            //     currY = abs(1 - modf(curr_posY, &syf));
-            //     fullY = 1;
-            // }
 
             int stepX, stepY;
-            if(deg >= 90 && deg < 270) stepX = -1;
-            else stepX = 1;
-            if(deg >= 0 && deg < 180) stepY = 1;
-            else stepY = -1;
+            if(deg >= 90 && deg < 270)
+            {
+                currX = abs(modf(curr_posX, &syf) / cos(to_rad(deg)));
+                stepX = -1;
+            }
+            else
+            {
+                currX = abs((1 - modf(curr_posX, &syf)) / cos(to_rad(deg)));
+                stepX = 1;
+            }
+            if(deg >= 0 && deg < 180)
+            {
+                currY = abs((1 - modf(curr_posY, &syf)) / sin(to_rad(deg)));
+                stepY = 1;
+            }
+            else
+            {
+                currY = abs(modf(curr_posY, &syf) / sin(to_rad(deg)));
+                stepY = -1;
+            }
             int mapX = long(curr_posX), mapY = long(curr_posY);
             int side;
             bool wall = 0;
@@ -261,8 +259,10 @@ void *game_loop(void *vargp)
             double dist;
             if(side == 0) dist = currX - fullX;
             else dist = currY - fullY;
+            // dist *= cos(to_rad(deg-curr_dir));
+
             int h = SCREEN_HEIGHT / dist;
-            int color = ((dist - 1) * COLORS) / (MAP_WIDTH + MAP_HEIGHT);
+            int color = COLORS - h * COLORS / SCREEN_HEIGHT;
             color = std::min(color, COLORS-1);
             color = std::max(color, 0);
             int line_beg = std::max(0, SCREEN_HEIGHT / 2 - h / 2);
